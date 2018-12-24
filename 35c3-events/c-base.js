4a7e139b6b92ff8463345670a0fd7682d026fe2d
Henri Bergius <henri.bergius@iki.fi>
2018-12-24 16:59:11 +0100

    Add c-base local events to calendar

---
+++ 35c3-events/c-base.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    allDay: true,
+    start: '2018-12-25T15:24:51+0000',
+    end: '2018-12-25T15:24:51+0000',
+    title: 'Alle Safeties checken',
+    id: 'c-base',
+  },
+  {
+    allDay: true,
+    start: '2018-12-25T15:24:51+0000',
+    end: '2018-12-25T15:24:51+0000',
+    title: 'Tische aufbauen',
+    id: 'c-base',
+  },
+  {
+    allDay: true,
+    start: '2018-12-25T15:24:51+0000',
+    end: '2018-12-25T15:24:51+0000',
+    title: 'Monitore anhängen mit dazz)',
+    id: 'c-base',
+  },
+  {
+    allDay: true,
+    start: '2018-12-25T15:24:51+0000',
+    end: '2018-12-25T15:24:51+0000',
+    title: 'projection mapping von riot platzieren',
+    id: 'c-base',
+  },
+  {
+    allDay: false,
+    start: '2018-12-25T18:00:00+0000',
+    end: '2018-12-25T18:00:00+0000',
+    title: 'Abnahme',
+    id: 'c-base',
+  },
+];
