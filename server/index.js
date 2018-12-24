0978d1df8de20caf1e858d74281ea538998ac46c
Henri Bergius <henri.bergius@iki.fi>
2018-12-24 14:23:22 +0100

    Proxy the fahrplan

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ const Static = require('koa-static');
 const Cors = require('koa-cors');
 const path = require('path');
 
+const route35c3 = require('./route/35c3');
+
 const app = new Koa();
 const router = new Router();
+
+route35c3(router);
 
 app
   .use(Cors())
