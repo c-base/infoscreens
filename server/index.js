e5245d82a03583c2fe4795592aea7d73b07aaa69
Henri Bergius <henri.bergius@iki.fi>
2018-12-24 14:08:36 +0100

    Enable CORS

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const Static = require('koa-static');
+const Cors = require('koa-cors');
 const path = require('path');
 
 const app = new Koa();
 const router = new Router();
 
 app
+  .use(Cors())
   .use(router.routes())
   .use(router.allowedMethods())
   .use(Static(path.resolve(__dirname, '../dist'), {}));
