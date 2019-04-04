47c36be218c44767d1e2ad7107d82b855599b659
Philip Stewart <philip.d.stewart@gmail.com>
2019-04-04 14:28:44 +0200

    Remove comment

--- server/route/cbeamRpc.js
+++ server/route/cbeamRpc.js
@@ -11,7 +11,6 @@ module.exports = (router) => {
       method: 'POST',
       mode: 'cors',
     });
-    // const res = await fetch('http://c-beam.cbrp3.c-base.org:4254/rpc/;, {"credentials":"omit","headers":{"content-type":"application/json"},"referrerPolicy":"no-referrer-when-downgrade","body":`{\"jsonrpc\":\"2.0\",\"id\":1550009052773,\"method\":\"${ctx.params.method}\",\"params\":\"\"}`,"method":"POST","mode":"cors"});
     ctx.assert((res.status === 200), res.status);
     const body = await res.json();
     ctx.body = body;
